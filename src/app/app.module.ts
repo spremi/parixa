71588bfad58d1990aa01a00e4d9694a1599f183a
Sanjeev Premi <spremi@ymail.com>
2019-09-09 01:28:20 +0530

    chore: app: parts: Add 'menu' component
    Represents custom menu component.
    
    Command:
      ng g c parts/menu --style=sass --lintFix=true
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { StockModule } from './modules/stock/stock.module';
+import { MenuComponent } from './parts/menu/menu.component';
 import { PilotComponent } from './parts/pilot/pilot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PilotComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
