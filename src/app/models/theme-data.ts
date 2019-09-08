49d498a4f3890274587c900d255e2c53b15d381b
Sanjeev Premi <spremi@ymail.com>
2019-09-09 01:56:40 +0530

    feat: models: theme-data: Initial definition
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/models/theme-data.ts
+++ src/app/models/theme-data.ts
@@ -1,2 +1,19 @@
 export interface ThemeData {
+  /** Name of the theme. */
+  name: string;
+
+  /** User friendly label of theme (for selection menu). */
+  label: string;
+
+  /** Primary color of the theme (for visualization). */
+  primary: string;
+
+  /** Accent color of the theme (for visualization). */
+  accent: string;
+
+  /** Warn color of the theme (for visualization). */
+  warn: string;
+
+  /** Is it dark theme? */
+  isDark: boolean;
 }
