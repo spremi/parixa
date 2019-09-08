123dd6a0a559ee0463e32bc9a224acb74b10ea83
Sanjeev Premi <spremi@ymail.com>
2019-09-09 01:23:26 +0530

    feat: app: Import HttpClient module
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { StockModule } from './modules/stock/stock.module';
@@ -12,6 +13,7 @@ import { StockModule } from './modules/stock/stock.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     StockModule,
   ],
   providers: [],
