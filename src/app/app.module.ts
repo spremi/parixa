ba174a3f94f222c040ec0d238cd1d2451da57c41
Sanjeev Premi <spremi@ymail.com>
2019-09-09 00:59:24 +0530

    chore: app: Add dependency on Angular Material
    Command:
      ng add @angular/material
      ng lint --fix=true
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
