71923179ec8f8ad451ae1f19a5f7578c8d55432e
Sanjeev Premi <spremi@ymail.com>
2019-09-09 01:26:24 +0530

    chore: app: parts: Add 'pilot' component
    Completely custom component that will be used to
    make theme selection.
    
    Command:
      ng g c parts/pilot --style=sass --lintFix=true
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { StockModule } from './modules/stock/stock.module';
+import { PilotComponent } from './parts/pilot/pilot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PilotComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/parts/pilot/pilot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sp-pilot',
+  templateUrl: './pilot.component.html',
+  styleUrls: ['./pilot.component.sass'],
+})
+export class PilotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
