c6e4530ee378bf5d2a623af24c3e31f5a90508c9
Sanjeev Premi <spremi@ymail.com>
2019-09-09 21:16:04 +0530

    feat: parts: menu: Initial implementation
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/menu/menu.component.ts
+++ src/app/parts/menu/menu.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ThemeData } from 'src/app/models/theme-data';
+import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
   selector: 'sp-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.sass'],
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private sub: Subscription;
+
+  /**
+   * List of themes.
+   */
+  themes: ThemeData[] = [];
+
+  constructor(private themeSvc: ThemeService) { }
 
   ngOnInit() {
+    this.sub = this.themeSvc.list().subscribe((result) => {
+      this.themes = result;
+    });
   }
 
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
 }
