706cfa1e22292b10929aacf6610e1517b8905a21
Sanjeev Premi <spremi@ymail.com>
2019-09-09 22:03:54 +0530

    feat: parts: option: Show theme colors
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/parts/option/option.component.ts
+++ src/app/parts/option/option.component.ts
@@ -1,4 +1,6 @@
-import { Component, HostListener, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit, Component, ElementRef, HostListener, Input, Renderer2, ViewChild
+} from '@angular/core';
 import { ThemeData } from 'src/app/models/theme-data';
 import { ThemeService } from 'src/app/services/theme.service';
 
@@ -7,12 +9,48 @@ import { ThemeService } from 'src/app/services/theme.service';
   templateUrl: './option.component.html',
   styleUrls: ['./option.component.sass'],
 })
-export class OptionComponent implements OnInit {
+export class OptionComponent implements AfterViewInit {
   @Input() data: ThemeData;
 
-  constructor(private themeSvc: ThemeService) { }
+  @ViewChild('primary', { static: false })
+  boxPrimary: ElementRef<HTMLSpanElement>;
 
-  ngOnInit() {
+  @ViewChild('accent', { static: false })
+  boxAccent: ElementRef<HTMLSpanElement>;
+
+  @ViewChild('warn', { static: false })
+  boxWarn: ElementRef<HTMLSpanElement>;
+
+  constructor(
+    private renderer: Renderer2,
+    private themeSvc: ThemeService
+  ) { }
+
+  ngAfterViewInit() {
+    if (!this.data) {
+      return;
+    }
+
+    if (this.data.primary && this.boxPrimary) {
+      this.renderer.setStyle(
+        this.boxPrimary.nativeElement,
+        'background-color',
+        this.data.primary);
+    }
+
+    if (this.data.accent && this.boxAccent) {
+      this.renderer.setStyle(
+        this.boxAccent.nativeElement,
+        'background-color',
+        this.data.accent);
+    }
+
+    if (this.data.warn && this.boxWarn) {
+      this.renderer.setStyle(
+        this.boxWarn.nativeElement,
+        'background-color',
+        this.data.warn);
+    }
   }
 
   @HostListener('click')
