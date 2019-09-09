0094a88a53b71a22b21a1867a93260dd82f2e73c
Sanjeev Premi <spremi@ymail.com>
2019-09-09 20:36:15 +0530

    feat: services: theme: Get list of themes
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { filter, tap } from 'rxjs/operators';
+import { ThemeData } from '../models/theme-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
 
-  constructor() { }
+  private themes: ThemeData[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  list(): Observable<ThemeData[]> {
+    if (this.themes.length > 0) {
+      return of(this.themes);
+    }
+
+    return this.http.get<ThemeData[]>('/assets/data/themes.json').pipe(
+      filter((data) => !!data),
+      tap((data) => {
+        this.themes = data;
+      })
+    );
+  }
 }
